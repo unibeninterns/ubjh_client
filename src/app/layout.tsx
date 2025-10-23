552804556548abb4813fe42c8ab4e91d24793573
lyteking <lightebube751@gmail.com>
2025-10-23 18:24:03 +0100

    Mobile view updted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,11 +59,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="w-full overflow-x-hidden">
+    <html lang="en" className="w-full">
       <body
-        className={`${inter.variable} ${merriweather.variable} ${jetbrainsMono.variable} antialiased w-full overflow-x-hidden`}
+        className={`${inter.variable} ${merriweather.variable} ${jetbrainsMono.variable} antialiased w-full`}
       >
-        <div className="max-w-[100vw] overflow-x-hidden">{children}</div>
+        <div className="max-w-[100vw]">{children}</div>
       </body>
     </html>
   );
